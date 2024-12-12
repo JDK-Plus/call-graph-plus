2990ab63c2e8eefa05165b8c11d42dfdbb42da0b
GitHub Action <action@github.com>
2024-12-12 16:04:37 +0000

    Template cleanup

--- src/test/kotlin/com/github/pmoon/callgraphplus/MyPluginTest.kt
+++ src/test/kotlin/com/github/pmoon/callgraphplus/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pmoon.callgraphplus
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pmoon.callgraphplus.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
